e31cd884d3b69e59dc0cd6693bee83d202c58ff4
Bowden <rwbowden@labs.neit.edu>
2018-01-18 12:47:32 -0500

    More files

--- updatecollection.js
+++ updatecollection.js
@@ -6,13 +6,13 @@ MongoClient.connect(url, function(err, db){
 	if(err) throw err;
 	
     var dbObj = db.db("mydb");
-    var querydata = {genre: "Not Overwatch"};
-    var newdata = {genre: "Better Battleborn"};
-    var update = {$set:newdata};
-
-    dbObj.collections("games").updateOne(querydata, update, function(err, res){
-        if (err) throw err;
-        console.log("Document updated.");
-        db.close();
-    });
+	var querydata = {genre: "Not Overwatch"};
+	var newdata = {genre: "Better Battleborn"};
+	var update = {$set:newdata};
+	
+	dbObj.collection("games").updateOne(querydata, update, function(err, res){
+		if(err)throw err;
+		console.log( "Document Updated");
+		db.close();
+	});
 });
